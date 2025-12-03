931347cbc5d7ea34805cd113fee1aa35d3afc1e6
testing <michael231me@gmail.com>
2025-12-03 04:05:10 +0100

    resolved /searchId issue

--- app/searchId/page.tsx
+++ app/searchId/page.tsx
@@ -1,12 +1,16 @@
 import Form from "@/component/form";
 import ContactPage from "@/component/formAction";
 import Search from "@/component/search";
+import { Suspense } from "react";
 
 export default async function searchPage() {
 
     return (
         <div className=" text-white ">
-            <Search />
+            <Suspense fallback={<div> Loading...</div>}>
+                <Search />
+            </Suspense>
+
             <Form />
             <ContactPage />
         </div>
